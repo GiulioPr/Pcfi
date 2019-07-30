7e57984ee9117eae8efb336ac6741b18168ff0b5
Giulio Prevedello <gprevedl@umr168-curn-0-21-6963.curie.fr>
2019-07-30 18:04:17 +0200

    faild attempt to regression generalisation

---
+++ 1_code/importanceMatrixReg.py
@@ -0,0 +1,143 @@
+# function to generate the importance matrix (e.g. how important is each feature to the prediction of one class)
+
+import numpy as np
+
+def f_gini(v):
+    p = v/v.sum()
+    return np.multiply(p, 1-p).sum()
+
+def f_entropy(v):
+    return sps.entropy(pk=v)
+
+def f_misclassification(v):
+    p = v/v.sum()
+    return 1 - p.max()
+
+def f_mse(v):
+    p = v/v.sum()
+    return 1 - p.max()
+
+def calcImportanceMatrix(rf_clf):
+    """
+    Calculates the importance matrix of predictors for each class.
+
+    Arguments:
+        rf_clf - The random forest classifier to calculate the importance matrix for.
+        Must (?) be a RandomForestClassifier object. EXTENSIBLE TO ANY DECISION TREE CLASSIFIER FROM sklearn.tree?
+
+    Returns:
+        importance_matrix - The importance matrix with the importance of each predictor in predicting a class.
+        A n by m numpy array.
+
+    """
+
+    # get the number of classes being predicted by the random forest
+    classes = rf_clf.classes_
+    n_classes = len(classes)
+
+    # init storage for the predictor importances by classes by trees
+    importance_matrix = []
+
+    #dec_tree = rf_clf.estimators_[0]
+    for dec_tree in rf_clf.estimators_:
+
+        # get the criterion used to measure impurity
+        criterion = dec_tree.get_params()['criterion']
+        if criterion == 'gini':
+            f_impurity = f_gini
+        elif criterion == 'entropy':
+            f_impurity = f_entropy
+        elif criterion == 'misclassification':
+            f_impurity = f_misclassification
+        elif criterion == 'mse':
+            f_impurity = f_mse
+        else:
+            f_impurity = 0
+            print('Unassigned impurity measure')
+
+        # get the number of features and nodes in the tree
+        feature = dec_tree.tree_.feature
+        n_features = dec_tree.tree_.n_features
+        n_nodes = dec_tree.tree_.__getstate__()['node_count']
+        nodes = dec_tree.tree_.__getstate__()['nodes']
+        parent_node_ind = -np.ones(shape=n_nodes, dtype='<i8')
+        #parent_node_ind[0] = n_nodes + 1
+        #print(parent_node_ind)
+        for par_ind,node in enumerate(nodes):
+            if node[0] != -1:
+                parent_node_ind[node[0]] = par_ind
+            if node[1] != -1:
+                parent_node_ind[node[1]] = par_ind
+        #print(parent_node_ind)
+
+        # identify the leaves of the tree
+        is_leaves = np.array([node[0]==-1 and node[1]==-1 for node in nodes])
+        leaves_index = np.nonzero(is_leaves)[0]
+
+        values_sorted = dec_tree.tree_.__getstate__()['values']
+        #print ('nodes', nodes, len(nodes), len(values_sorted[:,0,:]))
+        node_pred = np.argmax(values_sorted[:,0,:], axis=1)
+        leaves_class_index = node_pred[is_leaves]
+        #for par_ind,node in enumerate(nodes):
+        #    print(par_ind,parent_node_ind[par_ind],is_leaves[par_ind],node,
+        #          values_sorted[par_ind], values_sorted[par_ind].sum())
+        node_unvisited = np.ones((n_classes, n_nodes), dtype=bool)
+        tree_importances = np.zeros((n_classes, n_features))
+        for leaf_i,leaf_c_i in zip(leaves_index,leaves_class_index):
+            current_i = parent_node_ind[leaf_i]
+            #print('START from leaf ', leaf_i, 'with class ', leaf_c_i)
+            #print('whose parent is ', current_i)
+            # walk the tree and calculate the importance of the predictor
+            while current_i != -1 and node_unvisited[leaf_c_i,current_i]:
+                current_node = nodes[current_i]
+                left_node = nodes[current_node['left_child']]
+                right_node = nodes[current_node['right_child']]
+                current_feature = current_node['feature']
+                ###NEW HERE
+                current_values = values_sorted[current_i,0,:]
+                left_values = values_sorted[current_node['left_child'],0,:]
+                right_values = values_sorted[current_node['right_child'],0,:]
+
+                current_values_class = np.array([
+                    current_values[leaf_c_i],
+                    current_values[np.arange(len(current_values)) != leaf_c_i].sum()
+                ])
+                left_values_class = np.array([
+                    left_values[leaf_c_i],
+                    left_values[np.arange(len(left_values)) != leaf_c_i].sum()
+                ])
+                right_values_class = np.array([
+                    right_values[leaf_c_i],
+                    right_values[np.arange(len(right_values)) != leaf_c_i].sum()
+                ])
+                #print(
+                #    current_values,
+                #    np.array([current_values[leaf_c_i],
+                #              current_values[np.arange(len(current_values))!=leaf_c_i].sum()])
+                #     )
+                #print(current_values.sum(), left_values.sum(), right_values.sum(),
+                #     left_values.sum()/current_values.sum(), right_values.sum()/current_values.sum(),
+                #      current_node['weighted_n_node_samples'], left_node['weighted_n_node_samples'],
+                #      right_node['weighted_n_node_samples']
+                #     )
+                tree_importances[leaf_c_i,current_feature] += (
+                        current_node['weighted_n_node_samples'] * f_impurity(current_values_class) -
+                        left_node['weighted_n_node_samples'] * f_impurity(left_values_class) -
+                        right_node['weighted_n_node_samples'] * f_impurity(right_values_class)
+                        )
+                #print('\n', current_node, (
+                #        current_node['weighted_n_node_samples'] * f_importance(current_values) -
+                #        left_node['weighted_n_node_samples'] * f_importance(left_values) -
+                #        right_node['weighted_n_node_samples'] * f_importance(right_values)
+                #        ))
+                ###
+                node_unvisited[leaf_c_i,current_i] = False
+                current_i = parent_node_ind[current_i]
+                #print('next current is ', current_i)
+        importance_matrix.append(tree_importances/nodes[0]['weighted_n_node_samples'])
+
+    # average the predictor importances for each class by all of the trees in the forest
+    importance_matrix = np.mean(importance_matrix, axis = 0)
+    #normalise importance over each class
+    importance_matrix = (importance_matrix.T / np.sum(importance_matrix, axis=1)).T
+    return(importance_matrix)
